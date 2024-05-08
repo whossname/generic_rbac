6c8065045b81b6b772f00dc2b50530949a7a8099
tyson <tyson.buzza@gmail.com>
2024-05-08 14:55:03 +0800

    fix baseUrl, formatting

--- example_frontend/next.config.js
+++ example_frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/rbac',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- example_frontend/pages/_app.tsx
+++ example_frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
-import { SessionProvider } from 'next-auth/react'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '../components/layout';
+import { SessionProvider } from 'next-auth/react';
 
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
